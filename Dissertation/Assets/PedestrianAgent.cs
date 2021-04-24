2cf885e663e090ff4d8ca8b69245876a62df2cbe
William Shaw <williamralphshaw@gmail.com>
2021-04-24 22:03:04 +0100

    Added better heuristic controls, this made me discover that there is no friction and thus the agent will not slow down, TODO, add restance and velocity, angular variants, accerlation caps

--- Dissertation/Assets/PedestrianAgent.cs
+++ Dissertation/Assets/PedestrianAgent.cs
@@ -7,13 +7,14 @@ using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 
 
-
-
 public class PedestrianAgent : Agent
 {
     public Text TextBox;
     public int goalCount = 0;
     Rigidbody rBody;
+    public bool resetAgent = false;
+
+    public DateTime period;
     void Start()
     {
         rBody = GetComponent<Rigidbody>();
@@ -25,22 +26,26 @@ public class PedestrianAgent : Agent
     public Transform Platform;
     public Transform Floor;
     
+
     
 
     public override void OnEpisodeBegin()
     {
+        period = DateTime.Now;
         TextBox.text = "Platforms reached: " + goalCount.ToString();
-        if (Math.Abs(this.transform.localPosition.x) > 25f | Math.Abs(this.transform.localPosition.z) > 25f)
+        if (Math.Abs(this.transform.localPosition.x) > 25f | Math.Abs(this.transform.localPosition.z) > 25f | resetAgent | this.StepCount >= 4990)
         {
             this.rBody.angularVelocity = Vector3.zero;
             this.rBody.velocity = Vector3.zero;
             this.transform.localPosition = new Vector3(0, 1f, 0);
             this.transform.rotation = new Quaternion(0f, 1f, 0f, 1f);
+            resetAgent = false;
         }
 
-        Platform.localPosition = new Vector3(UnityEngine.Random.value * 45f - 22.5f,
+
+        Platform.localPosition = new Vector3(UnityEngine.Random.value * 43f - 21.5f,
                                            0f,
-                                           UnityEngine.Random.value * 45f - 22.5f);
+                                           UnityEngine.Random.value * 43f - 21.5f);
         Floor.localPosition = new Vector3(Floor.localPosition.x, -0.05f, Floor.localPosition.z);
 
     }
@@ -50,60 +55,105 @@ public class PedestrianAgent : Agent
     public override void CollectObservations(VectorSensor sensor)
     {
         // Target and Agent positions
-        sensor.AddObservation(Platform.transform.position);
-        sensor.AddObservation(this.transform.localPosition);
+        sensor.AddObservation(Platform.transform.position.x);
+        sensor.AddObservation(Platform.transform.position.y);
+        sensor.AddObservation(Platform.transform.position.z);
+        sensor.AddObservation(this.transform.localPosition.x);
+        sensor.AddObservation(this.transform.localPosition.y);
+        sensor.AddObservation(this.transform.localPosition.z);
         //agent rotations
-        sensor.AddObservation(this.transform.rotation);
+        sensor.AddObservation(this.transform.rotation.x);
+        sensor.AddObservation(this.transform.rotation.y);
+        sensor.AddObservation(this.transform.rotation.z);
+        sensor.AddObservation(this.transform.rotation.w);
 
         sensor.AddObservation(Vector3.Distance(this.transform.localPosition, Platform.localPosition));
 
         // Agent velocity
         sensor.AddObservation(rBody.velocity.x);
         sensor.AddObservation(rBody.velocity.z);
+        sensor.AddObservation(rBody.angularVelocity.x);
+        sensor.AddObservation(rBody.angularVelocity.y);
+        sensor.AddObservation(rBody.angularVelocity.z);
+
 
     }
 
-    public float pedestrianForwardSpeed = 5;
-    public float pedestrianBackSpeed = 1f;
-    public float pedestrianSideSpeed = 1;
-    public float rotationSpeed = 5;
+    public float pedestrianForwardSpeed = 20f;
+    public float pedestrianBackSpeed = 10f;
+    public float pedestrianSideSpeed = 2f;
+    public float rotationSpeed = 20f;
 
     public override void OnActionReceived(float[] vectorAction)
+        // todo ADD FRICTION, RESITANCE AND CAPS ON VELOCITIES AND ANGULAR VELOCITIES.
     {
         Vector3 controlSignal = Vector3.zero;
-        controlSignal = vectorAction[0] * pedestrianForwardSpeed * 1f * transform.forward;
-        controlSignal += vectorAction[1] * pedestrianBackSpeed * -1f * transform.forward;
-        controlSignal += vectorAction[2] * pedestrianSideSpeed * 1f * transform.right;
-        controlSignal += vectorAction[3] * pedestrianSideSpeed * -1f * transform.right;
+        controlSignal = vectorAction[0] * pedestrianForwardSpeed * 1f * this.transform.forward;
+        controlSignal += vectorAction[1] * pedestrianBackSpeed * -1f * this.transform.forward;
+        controlSignal += vectorAction[2] * pedestrianSideSpeed * 1f * this.transform.right;
+        controlSignal += vectorAction[3] * pedestrianSideSpeed * -1f * this.transform.right;
 
-        rBody.AddForce(controlSignal , ForceMode.VelocityChange);
 
-        // rotate body
-        float rotationSignal = rotationSpeed * vectorAction[4] - rotationSpeed * vectorAction[5];
-        transform.RotateAround(this.transform.localPosition, Vector3.up, rotationSignal * Time.deltaTime);
+        float rotationSignal = rotationSpeed * vectorAction[4];
+        rotationSignal -= rotationSpeed * vectorAction[5];
+        transform.RotateAround(this.transform.localPosition, Vector3.up, rotationSignal);
+        rBody.AddForce(controlSignal);
         
 
         float distanceToPlatform = Vector3.Distance(this.transform.localPosition, Platform.localPosition);
 
-        if (distanceToPlatform < 2.0f)
+        if (distanceToPlatform < 3.54f)
         { 
             goalCount++;
-            SetReward(1.0f);
+            AddReward(10.0f);
+            resetAgent = true;
             EndEpisode();
         }
 
         if (Math.Abs(this.transform.localPosition.x) > 25f | Math.Abs(this.transform.localPosition.z) > 25f)
         {
-            SetReward(-0.1f);
+            AddReward(-0.1f);
+            resetAgent = true;
             EndEpisode();
         }
+
+        AddReward(-0.001f);
     }
 
     public override void Heuristic(float[] actionsOut)
     {
-        actionsOut[0] = Input.GetAxis("Horizontal");
-        actionsOut[1] = Input.GetAxis("Vertical");
+        //actionsOut[0] = Input.GetAxis("Horizontal");
+        //actionsOut[1] = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            actionsOut[0] = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            actionsOut[1] = 1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            actionsOut[2] = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            actionsOut[3] = 1;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            actionsOut[4] = 1;
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            actionsOut[5] = 1;
+        } else {}
     }
 
+    public override void Initialize()
+    {
+        this.MaxStep = 5000;
+    }
 
 }
