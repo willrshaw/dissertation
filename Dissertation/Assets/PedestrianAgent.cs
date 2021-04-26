2e8f66e3e8627f9962f30d3d35d7f224ca48df94
William Shaw <williamralphshaw@gmail.com>
2021-04-26 18:01:52 +0100

    Agent movement fixed, multiple agents added

--- Dissertation/Assets/PedestrianAgent.cs
+++ Dissertation/Assets/PedestrianAgent.cs
@@ -20,7 +20,10 @@ public class PedestrianAgent : Agent
         rBody = GetComponent<Rigidbody>();
 
         // Lock the rotation of the X and Z axis to keep capsule upright.
-        rBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionY;
+        rBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        //| RigidbodyConstraints.FreezePositionY
+
+        //rBody.drag = 20f;
     }
 
     public Transform Platform;
@@ -33,19 +36,24 @@ public class PedestrianAgent : Agent
     {
         period = DateTime.Now;
         TextBox.text = "Platforms reached: " + goalCount.ToString();
-        if (Math.Abs(this.transform.localPosition.x) > 25f | Math.Abs(this.transform.localPosition.z) > 25f | resetAgent | this.StepCount >= 4990)
+        if (Math.Abs(this.transform.localPosition.x) > 25f | Math.Abs(this.transform.localPosition.z) > 25f | resetAgent | this.StepCount >= 4990 | resetAgent)
         {
             this.rBody.angularVelocity = Vector3.zero;
             this.rBody.velocity = Vector3.zero;
-            this.transform.localPosition = new Vector3(0, 1f, 0);
+            this.transform.localPosition = new Vector3(UnityEngine.Random.value * 10f + 12f, 1f, UnityEngine.Random.value * -10f - 18f);
             this.transform.rotation = new Quaternion(0f, 1f, 0f, 1f);
             resetAgent = false;
         }
 
 
-        Platform.localPosition = new Vector3(UnityEngine.Random.value * 43f - 21.5f,
+        //Platform.localPosition = new Vector3(UnityEngine.Random.value * 43f - 21.5f,
+        //                                 0f,
+        //                               UnityEngine.Random.value * 43f - 21.5f);
+
+        Platform.localPosition = new Vector3(-18f,
                                            0f,
-                                           UnityEngine.Random.value * 43f - 21.5f);
+                                           18f);
+
         Floor.localPosition = new Vector3(Floor.localPosition.x, -0.05f, Floor.localPosition.z);
 
     }
@@ -55,9 +63,9 @@ public class PedestrianAgent : Agent
     public override void CollectObservations(VectorSensor sensor)
     {
         // Target and Agent positions
-        sensor.AddObservation(Platform.transform.position.x);
-        sensor.AddObservation(Platform.transform.position.y);
-        sensor.AddObservation(Platform.transform.position.z);
+        sensor.AddObservation(Platform.transform.localPosition.x);
+        sensor.AddObservation(Platform.transform.localPosition.y);
+        sensor.AddObservation(Platform.transform.localPosition.z);
         sensor.AddObservation(this.transform.localPosition.x);
         sensor.AddObservation(this.transform.localPosition.y);
         sensor.AddObservation(this.transform.localPosition.z);
@@ -79,25 +87,9 @@ public class PedestrianAgent : Agent
 
     }
 
-    public float pedestrianForwardSpeed = 20f;
-    public float pedestrianBackSpeed = 10f;
-    public float pedestrianSideSpeed = 2f;
-    public float rotationSpeed = 20f;
-
+    
     public override void OnActionReceived(float[] vectorAction)
-        // todo ADD FRICTION, RESITANCE AND CAPS ON VELOCITIES AND ANGULAR VELOCITIES.
-    {
-        Vector3 controlSignal = Vector3.zero;
-        controlSignal = vectorAction[0] * pedestrianForwardSpeed * 1f * this.transform.forward;
-        controlSignal += vectorAction[1] * pedestrianBackSpeed * -1f * this.transform.forward;
-        controlSignal += vectorAction[2] * pedestrianSideSpeed * 1f * this.transform.right;
-        controlSignal += vectorAction[3] * pedestrianSideSpeed * -1f * this.transform.right;
-
-
-        float rotationSignal = rotationSpeed * vectorAction[4];
-        rotationSignal -= rotationSpeed * vectorAction[5];
-        transform.RotateAround(this.transform.localPosition, Vector3.up, rotationSignal);
-        rBody.AddForce(controlSignal);
+    { 
         
 
         float distanceToPlatform = Vector3.Distance(this.transform.localPosition, Platform.localPosition);
@@ -105,7 +97,7 @@ public class PedestrianAgent : Agent
         if (distanceToPlatform < 3.54f)
         { 
             goalCount++;
-            AddReward(10.0f);
+            SetReward(5.0f);
             resetAgent = true;
             EndEpisode();
         }
@@ -117,38 +109,88 @@ public class PedestrianAgent : Agent
             EndEpisode();
         }
 
-        AddReward(-0.001f);
+        AddReward(-0.01f);
+
+        MoveAgent(vectorAction);
+    }
+
+    public void MoveAgent(float[] act)
+    {
+        var dirToGo = Vector3.zero;
+        var rotateDir = Vector3.zero;
+
+        var forwardAxis = (int)act[0];
+        var rightAxis = (int)act[1];
+        var rotateAxis = (int)act[2];
+
+        float pedestrianForwardSpeed = 1f;
+        float pedestrianBackSpeed = 1f;
+        float pedestrianSideSpeed = 0.3f;
+        float rotationSpeed = 100f;
+
+        switch (forwardAxis)
+        {
+            case 1:
+                dirToGo = transform.forward * pedestrianForwardSpeed;
+                break;
+            case 2:
+                dirToGo = transform.forward * -pedestrianBackSpeed;
+                break;
+        }
+
+        switch (rightAxis)
+        {
+            case 1:
+                dirToGo = transform.right * pedestrianSideSpeed;
+                break;
+            case 2:
+                dirToGo = transform.right * -pedestrianSideSpeed;
+                break;
+        }
+
+        switch (rotateAxis)
+        {
+            case 1:
+                rotateDir = transform.up * -1f;
+                break;
+            case 2:
+                rotateDir = transform.up * 1f;
+                break;
+        }
+
+        transform.Rotate(rotateDir, Time.deltaTime * rotationSpeed);
+        rBody.AddForce(dirToGo, ForceMode.VelocityChange);
     }
 
     public override void Heuristic(float[] actionsOut)
     {
-        //actionsOut[0] = Input.GetAxis("Horizontal");
-        //actionsOut[1] = Input.GetAxis("Vertical");
-
+        //forward
         if (Input.GetKey(KeyCode.W))
         {
-            actionsOut[0] = 1;
+            actionsOut[0] = 1f;
         }
-        else if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
-            actionsOut[1] = 1;
+            actionsOut[0] = 2f;
         }
-        else if (Input.GetKey(KeyCode.D))
+        //rotate
+        if (Input.GetKey(KeyCode.A))
         {
-            actionsOut[2] = 1;
+            actionsOut[2] = 1f;
         }
-        else if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.D))
         {
-            actionsOut[3] = 1;
+            actionsOut[2] = 2f;
         }
-        else if (Input.GetKey(KeyCode.E))
+        //right
+        if (Input.GetKey(KeyCode.E))
         {
-            actionsOut[4] = 1;
+            actionsOut[1] = 1f;
         }
-        else if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.Q))
         {
-            actionsOut[5] = 1;
-        } else {}
+            actionsOut[1] = 2f;
+        }
     }
 
     public override void Initialize()
